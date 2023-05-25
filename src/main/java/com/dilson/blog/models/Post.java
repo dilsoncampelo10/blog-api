d15c19e0aa66ac5fd6e4c36564352c5a60ff3f13
dilsoncampelo10 <dilsono316@gmail.com>
2023-05-25 17:16:58 -0300

    post model

---
+++ src/main/java/com/dilson/blog/models/Post.java
@@ -0,0 +1,34 @@
+package com.dilson.blog.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = true, columnDefinition = "TEXT")
+    private String description;
+
+    //private List<Comment> comment;
+
+}
